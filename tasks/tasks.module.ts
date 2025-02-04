fdc52df469e7f6449ad5094225266c58e1e8c868
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2025-02-04 12:26:37 +0100

    change place of old files

--- tasks/tasks.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TasksService } from './tasks.service';
-import { RentalRepository } from 'src/rental/rental.repository';
-import { PrismaService } from 'src/prisma/prisma.service';
-
-@Module({
-  providers: [TasksService, RentalRepository, PrismaService],
-})
-export class TasksModule {}
